aefd63e2c777c6d497868a8ead594cf8c43b15f3
Kyle F <professionallyfree@gmail.com>
2024-02-28 12:14:18 -0600

    Added db js files

---
+++ mongo.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose")
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect("mongodb://127.0.0.1:27017/LogIn")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log(e);
+})
+
+const logInSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection
+
